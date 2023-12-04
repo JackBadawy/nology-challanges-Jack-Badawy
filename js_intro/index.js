7b5f4d4f2912c4b0763345e4e73d9b4fefdcaf07
Jack <jackbadawy1999@gmail.com>
2023-12-04 11:43:53 +1100

    js intro challenge

---
+++ js_intro/index.js
@@ -0,0 +1,30 @@
+console.log("it's working");
+
+//primitive data types
+// a primitive data type is aa data type that cant be broken down further (basic building block)
+
+//challange
+/**
+ * 1. number
+ * 2. number
+ * 3. null //number
+ * 4. string
+ * 5. boolean
+ * 6. boolean
+ * 7. string
+ * 8. string //number
+ * 9. string
+ * 10. string //number
+ * 11. string
+ * 12. null  //number
+ * 13. null //number
+ */
+
+//variable challellenge
+const firstName = "Jack";
+const lastName = "Badawy";
+const age = "24";
+
+console.log(
+  `Hi, my name is ${firstName} ${lastName} and I am ${age} years old`
+);
